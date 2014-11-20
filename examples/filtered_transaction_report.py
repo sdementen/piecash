e46aa7513aaf234ed52cc1e5b53ec993c8493bab
sdementen <sdementen@gmail.com>
2014-11-20 15:06:00 +0100

    - add jinja2 version of the report

--- examples/filtered_transaction_report.py
+++ examples/filtered_transaction_report.py
@@ -3,7 +3,7 @@ import re
 from piecash import Transaction, open_book
 
 
-s = open_book("multiple_Accounts.gnucash", open_if_lock=True)
+s = open_book("book.gnucash", open_if_lock=True)
 
 regex = re.compile("^/Rental/")
 
@@ -13,11 +13,27 @@ transactions = [tr for tr in s.transactions  # query all transactions in the boo
                     or any(regex.search(spl.memo) for spl in tr.splits))    # or memo field of any split of transaction
                 and tr.post_date.date() >= datetime.date(2014, 11, 1)]      # and with post_date no later than begin nov.
 
+
+# output report with simple 'print'
 print "Here are the transactions for the search criteria '{}':".format(regex.pattern)
 for tr in transactions:
     print "- {:%Y/%m/%d} : {}".format(tr.post_date, tr.description)
     for spl in tr.splits:
-        print "\t{amount}  {direction}  {account} {memo}".format(amount=abs(spl.value),
+        print "\t{amount}  {direction}  {account} : {memo}".format(amount=abs(spl.value),
                                                                  direction="-->" if spl.value > 0 else "<--",
                                                                  account=spl.account.fullname(),
                                                                  memo=spl.memo)
+
+# same with jinja2 templates
+import jinja2
+env = jinja2.Environment(trim_blocks=True, lstrip_blocks=True)
+print env.from_string("""
+Here are the transactions for the search criteria '{{regex.pattern}}':
+{% for tr in transactions %}
+- {{ tr.post_date.strftime("%Y/%m/%d") }} : {{ tr.description }}
+  {% for spl in tr.splits %}
+    {{ spl.value.__abs__() }} {% if spl.value < 0 %} --> {% else %} <-- {% endif %} {{ spl.account.fullname() }} : {{ spl.memo }}
+  {% endfor %}
+{% endfor %}
+""").render(transactions=transactions,
+            regex=regex)
