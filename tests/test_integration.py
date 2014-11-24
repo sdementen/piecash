e7e2ba354a3529cd9b3a222b1b3af7fb93970022
sdementen <sdementen@gmail.com>
2014-11-24 22:49:14 +0100

    - add as integration tests the example of the README

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -5,6 +5,7 @@
 # from pytest.mark import parametrize
 #
 import datetime
+import os
 
 import pytest
 
@@ -13,6 +14,7 @@ import pytest
 
 # parametrize = pytest.mark.parametrize
 from piecash import create_book, Account
+from piecash.model_common import GnucashException
 
 
 @pytest.fixture
@@ -66,3 +68,43 @@ class TestIntegration_EmptyBook(object):
                 parent=session.book.root_account)
         session.save()
         assert len(session.accounts)==3
+
+    def test_example(self):
+        import piecash
+
+        # open a GnuCash Book
+        test_folder = os.path.dirname(os.path.realpath(__file__))
+        file_template = os.path.join(test_folder, "test_book.gnucash")
+
+        session = piecash.open_book(file_template, readonly=True)
+        book = session.book
+        session=session.sa_session
+
+        # example 1, print all stock prices in the Book
+        # display all prices
+        for price in session.query(piecash.Price).all():
+            print "{}/{} on {} = {} {}".format(price.commodity.namespace,
+                                               price.commodity.mnemonic,
+                                               price.date,
+                                               float(price.value_num) / price.value_denom,
+                                               price.currency.mnemonic,
+                                               )
+
+        for account in session.query(piecash.Account).all():
+            print account
+
+        # build map between account fullname (e.g. "Assets:Current Assets" and account)
+        map_fullname_account = {account.fullname():account for account in session.query(piecash.Account).all()}
+
+        # use it to retrieve the current assets account
+        acc_cur = map_fullname_account["Assets:Current Assets"]
+
+        # retrieve EUR currency
+        EUR = session.query(piecash.Commodity).filter_by(mnemonic='EUR').one()
+
+        # add a new subaccount to this account of type ASSET with currency EUR
+        piecash.Account(name="new savings account", account_type="ASSET", parent=acc_cur, commodity=EUR)
+
+        # save changes
+        with pytest.raises(GnucashException) as excinfo:
+            session.commit()
