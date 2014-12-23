902f39517ed56a63719daddb262c8ed2050ece44
sdementen <sdementen@gmail.com>
2014-12-23 21:50:45 +0100

    remove "/" in the project string

--- piecash/metadata.py
+++ piecash/metadata.py
@@ -6,7 +6,7 @@ Information describing the project.
 
 # The package name, which is also the "UNIX name" for the project.
 package = 'piecash'
-project = "Python interface/bindings to GnuCash SQL documents"
+project = "Python interface to GnuCash documents"
 project_no_spaces = project.replace(' ', '')
 version = '0.3.0'
 description = 'A pythonic interface to GnuCash SQL documents.'
