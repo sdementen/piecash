701bd32fa49407b5aa085b8627873d044ea70592
sdementen <sdementen@gmail.com>
2015-11-12 21:45:16 +0100

    update appveyor to move DB creation of mysql to test_script rework db uri and config for appveyor

--- tests/test_helper.py
+++ tests/test_helper.py
@@ -12,52 +12,61 @@ file_for_test = os.path.join(test_folder, "empty_book_for_test.gnucash")
 file_template_full = os.path.join(book_folder, "test_book.gnucash")
 file_for_test_full = os.path.join(test_folder, "test_book_for_test.gnucash")
 
-if sys.version_info.major==3:
+if sys.version_info.major == 3:
     def run_file(fname):
         with open(fname) as f:
             code = compile(f.read(), fname, 'exec')
-            exec(code, {})
+            exec (code, {})
 else:
     def run_file(fname):
         return execfile(fname, {})
 
 db_sqlite = os.path.join(test_folder, "fooze.sqlite")
-if os.environ.get("TRAVIS", False):
-    pg_password = ''
-else:
-    pg_password = os.environ.get("PG_PASSWORD")
 
-db_postgres_uri = "postgresql://postgres:{pwd}@localhost:5432/foo".format(pwd=pg_password)
-db_mysql_uri = "mysql+pymysql://travis:@localhost/foo?charset=utf8"
+TRAVIS = os.environ.get("TRAVIS", False)
+APPVEYOR = os.environ.get("APPVEYOR", False)
+LOCALSERVER = os.environ.get("PIECASH_DBSERVER_TEST", False)
+
 db_sqlite_uri = "sqlite:///{}".format(db_sqlite)
-
+databases_to_check = [None,
+                      db_sqlite_uri ]
 db_config = {
-    "postgres" : dict(db_type="postgres", db_name="foo",
-                      db_user="postgres", db_password=pg_password,
-                      db_host="localhost", db_port=5432),
-    "mysql" : dict(db_type="mysql", db_name="foo",
-                      db_user="travis", db_password="",
-                      db_host="localhost", db_port=3306),
     "sqlite": dict(sqlite_file=db_sqlite),
     "sqlite_in_mem": dict(sqlite_file=None),
 }
 
-
-databases_to_check = [None, db_sqlite_uri]
-if os.environ.get("TRAVIS", False) or os.environ.get("APPVEYOR", False):
-    databases_to_check.append(db_postgres_uri)
-    databases_to_check.append(db_mysql_uri)
-elif os.environ.get("PIECASH_DBSERVER_TEST", False):
-    databases_to_check.append(db_postgres_uri)
-    databases_to_check.append(db_mysql_uri)
-    db_config.pop("mysql")
+if TRAVIS or LOCALSERVER:
+    pg_password = os.environ.get("PG_PASSWORD","")
+    db_user = "travis"
+    databases_to_check.append("postgresql://postgres:{pwd}@localhost:5432/foo".format(pwd=pg_password))
+    databases_to_check.append("mysql+pymysql://travis:@localhost/foo?charset=utf8")
+    db_config.update({
+        "postgres": dict(db_type="postgres", db_name="foo",
+                         db_user="postgres", db_password=pg_password,
+                         db_host="localhost", db_port=5432),
+        "mysql": dict(db_type="mysql", db_name="foo",
+                      db_user="travis", db_password="",
+                      db_host="localhost", db_port=3306),
+    })
+elif APPVEYOR:
+    databases_to_check.append("postgresql://postgres:Password12!@localhost:5432/foo")
+    databases_to_check.append("mysql+pymysql://root:Password12!@localhost/foo?charset=utf8")
+    db_config.update({
+        "postgres": dict(db_type="postgres", db_name="foo",
+                         db_user="postgres", db_password="Password12!",
+                         db_host="localhost", db_port=5432),
+        "mysql": dict(db_type="mysql", db_name="foo",
+                      db_user="root", db_password="Password12!",
+                      db_host="localhost", db_port=3306),
+    })
 else:
-    db_config.pop("mysql")
-    db_config.pop("postgres")
+    pass
+
 
 @pytest.yield_fixture(params=[Customer, Vendor, Employee])
 def Person(request):
-    yield  request.param
+    yield request.param
+
 
 @pytest.yield_fixture(params=db_config.items())
 def book_db_config(request):
@@ -66,14 +75,14 @@ def book_db_config(request):
     sql_backend, db_config = request.param
     name = build_uri(**db_config)
 
-    if sql_backend!="sqlite_in_mem" and database_exists(name):
+    if sql_backend != "sqlite_in_mem" and database_exists(name):
         drop_database(name)
 
     yield db_config
 
-
-    if sql_backend!="sqlite_in_mem"  and database_exists(name):
+    if sql_backend != "sqlite_in_mem" and database_exists(name):
         drop_database(name)
+
 
 @pytest.yield_fixture(params=databases_to_check[1:])
 def book_uri(request):
@@ -85,6 +94,7 @@ def book_uri(request):
 
     if name and database_exists(name):
         drop_database(name)
+
 
 @pytest.yield_fixture(params=databases_to_check)
 def new_book(request):
@@ -111,6 +121,7 @@ def new_book_USD(request):
     if name and database_exists(name):
         drop_database(name)
 
+
 @pytest.yield_fixture(params=databases_to_check)
 def book_basic(request):
     name = request.param
@@ -121,7 +132,7 @@ def book_basic(request):
     b = create_book(uri_conn=name, currency="EUR", keep_foreign_keys=False)
     # create some accounts
     curr = b.currencies[0]
-    cdty = Commodity(namespace=u"échange",mnemonic=u"ïoà", fullname=u"Example of unicode déta")
+    cdty = Commodity(namespace=u"échange", mnemonic=u"ïoà", fullname=u"Example of unicode déta")
     a = Account(name="asset", type="ASSET", commodity=curr, parent=b.root_account)
     Account(name="broker", type="STOCK", commodity=cdty, parent=a)
     Account(name="exp", type="EXPENSE", commodity=curr, parent=b.root_account)
