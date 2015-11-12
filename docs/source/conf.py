7777f505174b53cc76167f80b990463d131d066e
sdementen <sdementen@gmail.com>
2015-11-12 08:55:51 +0100

    change them in sphinx to fix RTD

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -131,6 +131,7 @@ if not on_rtd:  # only import and set the theme if we're building docs locally
 
 html_theme = 'nature'
 html_theme = "sphinx_rtd_theme"
+html_theme = "default"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
