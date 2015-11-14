3c8e6685570bcfccc5d6282ac29bcad5ef736f05
sdementen <sdementen@gmail.com>
2015-11-14 07:08:30 +0100

    use new declared_attr behavior for slots definition

--- piecash/_declbase.py
+++ piecash/_declbase.py
@@ -1,6 +1,7 @@
 import uuid
 
 from sqlalchemy import Column, VARCHAR, event
+from sqlalchemy.ext.declarative import declared_attr
 from sqlalchemy.orm import relation, foreign, object_session
 
 from .kvp import DictWrapper, Slot
@@ -14,18 +15,22 @@ class DeclarativeBaseGuid(DictWrapper, DeclarativeBase):
     #: the unique identifier of the object
     guid = Column('guid', VARCHAR(length=32), primary_key=True, nullable=False, default=lambda: uuid.uuid4().hex)
 
+    @declared_attr
+    def slots(cls):
+        rel = relation('Slot',
+                             primaryjoin=foreign(Slot.obj_guid) == cls.guid,
+                             cascade='all, delete-orphan',
+                             collection_class=CallableList,
+        )
+
+        return rel
+
     # set the relation to the slots table (KVP)
     @classmethod
     def __declare_last__(cls):
         # do not do it on the DeclarativeBaseGuid as it is an abstract class
         if cls == DeclarativeBaseGuid:
             return
-
-        cls.slots = relation('Slot',
-                             primaryjoin=foreign(Slot.obj_guid) == cls.guid,
-                             cascade='all, delete-orphan',
-                             collection_class=CallableList,
-        )
 
         # assign id of slot when associating to object
         @event.listens_for(cls.slots, "remove")
