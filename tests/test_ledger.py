f39ddf10e0626a3f7988b4728241947f91b246fd
Alen Siljak <alen.siljak@gmx.com>
2019-04-17 11:52:11 +0200

    adjusting the ledger test

--- tests/test_ledger.py
+++ tests/test_ledger.py
@@ -9,4 +9,5 @@ from test_helper import file_template_full
 class TestLedger_out_write(object):
     def test_out_write(self):
         with piecash.open_book(file_template_full, open_if_lock=True) as data:
-            sys.stdout.write(piecash.ledger(data))
+            #sys.stdout.write(piecash.ledger(data))
+            sys.stdout.write(piecash.get_ledger_output(data))
