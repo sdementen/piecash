524e8ea0b706208799b06ca74d0ec98e20d89f63
sdementen <sdementen@gmail.com>
2019-06-09 14:24:32 +0200

    support export to ledger with default locale use single dispatch for ledger export logic

--- piecash/ledger.py
+++ piecash/ledger.py
@@ -1,4 +1,8 @@
 from __future__ import unicode_literals
+
+import locale
+from functools import singledispatch
+
 from .core import Transaction, Account, Commodity, Price, Book
 
 """original script from https://github.com/MatzeB/pygnucash/blob/master/gnucash2ledger.py by Matthias Braun matze@braunis.de
@@ -7,41 +11,58 @@ from .core import Transaction, Account, Commodity, Price, Book
  - new string formatting
 """
 
-
-def attach_ledger(cls):
-    def _process(fct):
-        cls.__ledger__ = fct
-
-    return _process
+try:
+    from money import Money
+except ImportError:
+    Money = None
 
 
-@attach_ledger(Transaction)
-def ledger(tr):
+@singledispatch
+def ledger(obj, **kwargs):
+    raise NotImplemented
+
+
+@ledger.register(Transaction)
+def _(tr, **kwargs):
     """Return a ledger-cli alike representation of the transaction"""
-    s = ["{:%Y-%m-%d} {}{}\n".format(tr.post_date,
-                                       "({}) ".format(tr.num.replace(")", "")) if tr.num else "",
-                                       tr.description)]
+    s = [
+        "{:%Y-%m-%d} {}{}\n".format(
+            tr.post_date, "({}) ".format(tr.num.replace(")", "")) if tr.num else "", tr.description
+        )
+    ]
     if tr.notes:
         s.append("\t;{}\n".format(tr.notes))
     for split in tr.splits:
         if split.account.commodity.mnemonic == "template":
             return ""
-        if split.reconcile_state in ['c', 'y']:
+        if split.reconcile_state in ["c", "y"]:
             s.append("\t* {:38}  ".format(split.account.fullname))
         else:
             s.append("\t{:40}  ".format(split.account.fullname))
         if split.account.commodity != tr.currency:
-            s.append("{:10.{}f} {} @@ {:.{}f} {}".format(
-                split.quantity,
-                split.account.commodity.precision,
-                format_commodity(split.account.commodity),
-                abs(split.value),
-                tr.currency.precision,
-                format_commodity(tr.currency)))
+            s.append(
+                "{:10.{}f} {} @@ {:.{}f} {}".format(
+                    split.quantity,
+                    split.account.commodity.precision,
+                    format_commodity(split.account.commodity),
+                    abs(split.value),
+                    tr.currency.precision,
+                    format_commodity(tr.currency),
+                )
+            )
         else:
-            s.append("{:10.{}f} {}".format(split.value,
-                                           tr.currency.precision,
-                                           format_commodity(tr.currency)))
+            if kwargs.get("locale"):
+                _locale = locale.getdefaultlocale()[0]
+                if Money is None:
+                    raise ValueError(f"You must install Money ('pip install money') to export to ledger in your locale '{_locale}")
+                s.append(Money(amount=split.value, currency=format_commodity(tr.currency)).format(_locale))
+            else:
+                if Money:
+                    # vesion from Money
+                    s.append(str(Money(amount=split.value, currency=format_commodity(tr.currency))))
+                else:
+                    # local hand made version
+                    s.append("{:10.{}f} {}".format(split.value, tr.currency.precision, format_commodity(tr.currency)))
         if split.memo:
             s.append(" ;   {:20}".format(split.memo))
         s.append("\n")
@@ -52,15 +73,15 @@ def ledger(tr):
 def format_commodity(commodity):
     mnemonic = commodity.mnemonic
     try:
-        if mnemonic.encode('ascii').isalpha():
+        if mnemonic.encode("ascii").isalpha():
             return mnemonic
     except:
         pass
-    return "\"{}\"".format(mnemonic)  # TODO: escape " char in mnemonic
+    return '"{}"'.format(mnemonic)  # TODO: escape " char in mnemonic
 
 
-@attach_ledger(Commodity)
-def ledger(cdty):
+@ledger.register(Commodity)
+def _(cdty, **kwargs):
     """Return a ledger-cli alike representation of the commodity"""
     if cdty.mnemonic in ["", "template"]:
         return ""
@@ -71,56 +92,51 @@ def ledger(cdty):
     return res
 
 
-@attach_ledger(Account)
-def ledger(acc):
+@ledger.register(Account)
+def _(acc, **kwargs):
     """Return a ledger-cli alike representation of the account"""
     # ignore "dummy" accounts
     if acc.type is None or acc.parent is None:
         return ""
     if acc.commodity.mnemonic == "template":
         return ""
-    res = "account {}\n".format(acc.fullname, )
+    res = "account {}\n".format(acc.fullname)
     if acc.description != "":
-        res += "\tnote {}\n".format(acc.description, )
+        res += "\tnote {}\n".format(acc.description)
 
-    res += "\tcheck commodity == \"{}\"\n".format(format_commodity(acc.commodity).replace("\"", "\\\""))
+    res += '\tcheck commodity == "{}"\n'.format(format_commodity(acc.commodity).replace('"', '\\"'))
     return res
 
 
-@attach_ledger(Price)
-def ledger(price):
+@ledger.register(Price)
+def _(price, **kwargs):
     """Return a ledger-cli alike representation of the price"""
-    return "P {:%Y-%m-%d %H:%M:%S} {} {} {}\n".format(price.date,
-                                                      format_commodity(price.commodity),
-                                                      price.value,
-                                                      format_commodity(price.currency))
+    return "P {:%Y-%m-%d %H:%M:%S} {} {} {}\n".format(
+        price.date, format_commodity(price.commodity), price.value, format_commodity(price.currency)
+    )
 
 
-@attach_ledger(Book)
-def ledger(book):
+@ledger.register(Book)
+def _(book, **kwargs):
     """Return a ledger-cli alike representation of the book"""
     res = []
 
     # Commodities
     for commodity in book.commodities:
-        res.append(ledger(commodity))
+        res.append(ledger(commodity, **kwargs))
 
     # Accounts
     for acc in book.accounts:
-        res.append(ledger(acc))
+        res.append(ledger(acc, **kwargs))
         res.append("\n")
 
     # Prices
     for price in sorted(book.prices, key=lambda x: x.date):
-        res.append(ledger(price))
+        res.append(ledger(price, **kwargs))
     res.append("\n")
 
     for trans in sorted(book.transactions, key=lambda x: x.post_date):
-        res.append(ledger(trans))
+        res.append(ledger(trans, **kwargs))
         res.append("\n")
 
     return "".join(res)
-
-
-def ledger(obj):
-    return obj.__ledger__()
