8b9c5e09d2eb4fb2fa22017f3c18c1315157cc7d
sdementen <sdementen@gmail.com>
2019-06-09 19:02:30 +0200

    add money extra

--- piecash/scripts/ledger.py
+++ piecash/scripts/ledger.py
@@ -17,15 +17,18 @@ from piecash.scripts.cli import cli
 
 
 @cli.command()
-@click.argument('book', type=click.Path(exists=True))
-@click.option('--locale/--no-locale', default=False,
-              help="Export using locale for moyneyFile to which to export the data (default=stdout)")
-@click.option('--output', type=click.File('w', encoding="utf-8"), default="-",
-              help="File to which to export the data (default=stdout)")
+@click.argument("book", type=click.Path(exists=True))
+@click.option("--locale/--no-locale", default=False, help="Export currency amounts using locale for money")
+@click.option(
+    "--output",
+    type=click.File("w", encoding="utf-8"),
+    default="-",
+    help="File to which to export the data (default=stdout)",
+)
 def ledger(book, output, locale):
     """Export to ledger-cli format.
 
     This scripts export a GnuCash BOOK to the ledget-cli format.
     """
     with piecash.open_book(book, open_if_lock=True) as data:
-        output.write(piecash.ledger(data,locale=locale))
+        output.write(piecash.ledger(data, locale=locale))
