181aef6b5d7ab46bc1036e0139eac46759613976
sdementen <sdementen@gmail.com>
2020-10-20 10:08:00 +0200

    bump version to 1.2.0

--- piecash/metadata.py
+++ piecash/metadata.py
@@ -8,7 +8,7 @@ Information describing the project.
 package = 'piecash'
 project = "Python interface to GnuCash documents"
 project_no_spaces = project.replace(' ', '')
-version = '1.0.0'
+version = '1.2.0'
 description = 'A pythonic interface to GnuCash SQL documents.'
 authors = ['sdementen']
 authors_string = ', '.join(authors)
