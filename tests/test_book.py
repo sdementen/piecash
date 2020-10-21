d6897127cff00584664fc1e432fa215b4b70b662
sdementen <sdementen@users.noreply.github.com>
2020-10-21 08:02:18 +0200

    Fix uri of inexistent db

--- tests/test_book.py
+++ tests/test_book.py
@@ -185,12 +185,13 @@ class TestBook_open_book(object):
             assert b.default_currency.mnemonic == "EUR"
 
         # open book with checking existence
+        book_uri_fail = book_uri.replace("foo", "foofail")
         with pytest.raises(GnucashException, match="Database .* does not exist"):
-            open_book(uri_conn=book_uri + ".fail")
+            open_book(uri_conn=book_uri_fail)
 
         # open book without checking existence
         with pytest.raises(OperationalError):
-            open_book(uri_conn=book_uri + ".fail", check_exists=False)
+            open_book(uri_conn=book_uri_fail, check_exists=False)
 
     def test_open_RW_backup(self, book_uri):
         # create book
@@ -487,7 +488,7 @@ class TestBook_access_book(object):
         """
         assert (
             book_reference_3_0_0_fulloptions.business_company_address
-            == "Rue de la Chenille éclairée, 22"
+            == "Rue de la Chenille Ã©clairÃ©e, 22"
         )
         assert book_reference_3_0_0_fulloptions.business_company_contact == "John Michu"
         assert book_reference_3_0_0_fulloptions.business_company_email == "woozie@example.com"
@@ -514,18 +515,18 @@ class TestBook_access_book(object):
         Tests business slots
         :type book_reference_3_0_0_basic: Book
         """
-        book_reference_3_0_0_basic.business_company_address = "é"
-        book_reference_3_0_0_basic.business_company_contact = "à"
-        book_reference_3_0_0_basic.business_company_email = "ù"
-        book_reference_3_0_0_basic.business_company_ID = "ö"
-        book_reference_3_0_0_basic.business_company_name = "µ"
-        book_reference_3_0_0_basic.business_company_phone = "²"
-        book_reference_3_0_0_basic.business_company_website = "³"
+        book_reference_3_0_0_basic.business_company_address = "Ã©"
+        book_reference_3_0_0_basic.business_company_contact = "Ã "
+        book_reference_3_0_0_basic.business_company_email = "Ã¹"
+        book_reference_3_0_0_basic.business_company_ID = "Ã¶"
+        book_reference_3_0_0_basic.business_company_name = "Âµ"
+        book_reference_3_0_0_basic.business_company_phone = "Â²"
+        book_reference_3_0_0_basic.business_company_website = "Â³"
 
-        assert book_reference_3_0_0_basic.business_company_address == "é"
-        assert book_reference_3_0_0_basic.business_company_contact == "à"
-        assert book_reference_3_0_0_basic.business_company_email == "ù"
-        assert book_reference_3_0_0_basic.business_company_ID == "ö"
-        assert book_reference_3_0_0_basic.business_company_name == "µ"
-        assert book_reference_3_0_0_basic.business_company_phone == "²"
-        assert book_reference_3_0_0_basic.business_company_website == "³"
+        assert book_reference_3_0_0_basic.business_company_address == "Ã©"
+        assert book_reference_3_0_0_basic.business_company_contact == "Ã "
+        assert book_reference_3_0_0_basic.business_company_email == "Ã¹"
+        assert book_reference_3_0_0_basic.business_company_ID == "Ã¶"
+        assert book_reference_3_0_0_basic.business_company_name == "Âµ"
+        assert book_reference_3_0_0_basic.business_company_phone == "Â²"
+        assert book_reference_3_0_0_basic.business_company_website == "Â³"
